2aed492ed705039c3fc149a5c15a3ddf1737b03d
codriv <combodriver4@yahoo.at>
2021-10-28 23:19:32 +0200

    weeks03-lab-tasks  -  day02  -  Mathematics repaired

--- week03-lab-tasks/src/main/java/day02/Mathematics.java
+++ week03-lab-tasks/src/main/java/day02/Mathematics.java
@@ -2,11 +2,11 @@ package day02;
 
 public class Mathematics {
     public boolean isPrimeNumber(int number) {
-                for (int i = 2; i < number; i++) {
+        for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 return false;
             }
         }
-        return (number == 0 || number == 1 ? false : true);
+        return (number == 0 || number == 1) ? false : true;
     }
 }
